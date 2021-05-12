ef738b9136a21e17b9e98e4c86d7e16eae1f3679
Stefan Mihaylov <mihaylov.s@gmail.com>
2021-05-13 00:35:05 +0300

    update packages

--- Mihaylov.Web/Controllers/HomeController.cs
+++ Mihaylov.Web/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Mihaylov.GoogleDrive;
-using Mihaylov.GoogleDrive.Interfaces;
+﻿using System.Web.Mvc;
 using Mihaylov.Web.Common.Toastr;
 using Mihaylov.Web.Controllers.Base;
 using Ninject.Extensions.Logging;
@@ -13,37 +7,24 @@ namespace Mihaylov.Web.Controllers
 {
     public class HomeController : BaseController
     {
-      //  private IGoogleDriveApiHelper googleDrive;
- 
-        public HomeController(ILogger logger, IToastrHelper toaster)//, IGoogleDriveApiHelper google)
+        public HomeController(ILogger logger, IToastrHelper toaster)
             : base(logger, toaster)
         {
-           // this.googleDrive = google;
         }
 
         public ActionResult Index()
         {
-           // this.AddToastMessage("Congratulations", "You made it all the way here!", ToastType.Success);
-          
             return View();
         }
 
         public ActionResult About()
         {
-           // this.AddToastMessage("Redirected message", "This message has been redirected", ToastType.Error);
-
             return View();
         }
 
         public ActionResult Contact()
         {
             return View();
-        }
-
-        //http://localhost:57756/Home/geturl?folder=IMG101_0142_p_tiles&file=IMG101_0142_p_t_1f_01_01.jpg
-        public string GetUrl(string folder, string file)
-        {
-            return null; // this.googleDrive.GetUrl(folder, file);
         }
     }
 }
