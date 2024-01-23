ff711d3e341e48b999e357afb694fd457d364483
Stefan Mihaylov <mihaylov.s@gmail.com>
2024-01-23 11:51:58 +0200

    update in module info

--- Common/Mihaylov.Common.Host/AssemblyVersion/ModuleAssemblyService.cs
+++ Common/Mihaylov.Common.Host/AssemblyVersion/ModuleAssemblyService.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Runtime.Versioning;
 using Microsoft.Extensions.Options;
-using Mihaylov.Common.Host.BuildDate;
 using Mihaylov.Common.Host.AssemblyVersion.Interfaces;
 using Mihaylov.Common.Host.AssemblyVersion.Models;
 
@@ -11,28 +11,33 @@ namespace Mihaylov.Common.Host.AssemblyVersion
     public class ModuleAssemblyService : IModuleAssemblyService
     {
         private readonly Assembly _rootAssembly;
-        private readonly Assembly _currentAssembly;
 
         public ModuleAssemblyService(IOptions<AssemblyWrapper> currentAssembly)
         {
             _rootAssembly = currentAssembly.Value.Assembly;
-            _currentAssembly = Assembly.GetExecutingAssembly();
         }
 
         public ModuleInfo GetModuleInfo()
         {
             var assemblyName = _rootAssembly.GetName();
-            
-            var targetFramework = _rootAssembly.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkDisplayName;
-            targetFramework = !string.IsNullOrEmpty(targetFramework) ? targetFramework : assemblyName.Name;
-            
+
+            var framework = Environment.Version?.ToString();
+            if (string.IsNullOrEmpty(framework))
+            {
+                framework = _rootAssembly.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkDisplayName;
+
+                if (string.IsNullOrEmpty(framework))
+                {
+                    framework = assemblyName.Name;
+                }
+            }
+
             var name = _rootAssembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product ?? assemblyName.Name;
             var version = _rootAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? assemblyName.Version.ToString();
 
-            DateTime? buildDate = _currentAssembly.GetCustomAttribute<ReleaseDateAttribute>()?.ReleaseDate;
-            var buildDateStr = buildDate?.ToString("yyyy.MM.dd HH:mm") ?? string.Empty;
+            DateTime buildDate = new FileInfo(_rootAssembly.Location).LastWriteTimeUtc;
 
-            var result = new ModuleInfo(name, version, targetFramework, buildDateStr);
+            var result = new ModuleInfo(name, version, framework, buildDate.ToString("yyyy.MM.dd HH:mm"));
             return result;
         }
     }
